d828c0bd5c7af67d7e326e251728d6f0ffd3a745
ira.klotzko <ira.klotzko@d0613c50-22f8-11de-8b5c-13accb87f508>
2009-05-05 16:36:46 +0000

    got the ready request handler and monitor working

--- ReadyMonitor/source/com/appnexus/readymonitor/ReadyMonitor.java
+++ ReadyMonitor/source/com/appnexus/readymonitor/ReadyMonitor.java
@@ -5,8 +5,14 @@ import org.apache.log4j.Logger;
 import java.util.Properties;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.net.URLConnection;
 import java.net.URL;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -43,16 +49,31 @@ public class ReadyMonitor {
             this.targetURI = targetURI;
         }
         public void run() {
-//            URL url = new URL();
+            URL url;
+            try {
+                url = new URL(targetURI + "?hello=world");
+            } catch (MalformedURLException e) {
+                LOG.fatal("Could not create URL from=[" + targetURI + "] exiting monitor", e);
+                return;
+            }
             //noinspection InfiniteLoopStatement
             while(true) {
                 try {
-//                    URLConnection uc =
+                    URLConnection uc = url.openConnection();
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+                    StringBuffer sb = new StringBuffer();
+                    String line;
+                    while ((line = rd.readLine()) != null) {
+                        sb.append(line);
+                    }
+                    rd.close();
+                    String result = sb.toString();
+                    LOG.info("sent out a ping to targetURI=[" + targetURI + "] received=[" + result + "]");
                     Thread.sleep(pingTime);
                 } catch (InterruptedException ignored) {
+                } catch (IOException e) {
+                    LOG.error("Could not send request to=[" + targetURI + "] ", e);
                 }
-                // write out a pingee
-                LOG.info("sent out a ping to targetURI=[" + targetURI + "]");
             }
         }
     }
