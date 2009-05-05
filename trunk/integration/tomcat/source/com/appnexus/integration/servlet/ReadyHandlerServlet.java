2e074fbccb5e411884f3925519ae655900fb68a7
ira.klotzko <ira.klotzko@d0613c50-22f8-11de-8b5c-13accb87f508>
2009-05-05 16:36:46 +0000

    got the ready request handler and monitor working

--- trunk/integration/tomcat/source/com/appnexus/integration/servlet/ReadyHandlerServlet.java
+++ trunk/integration/tomcat/source/com/appnexus/integration/servlet/ReadyHandlerServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import java.io.IOException;
 
 /**
@@ -21,8 +23,25 @@ public class ReadyHandlerServlet extends HttpServlet {
     private static final Logger LOG = Logger.getLogger(ReadyHandlerServlet.class);
     @Override
     protected void service(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
-        System.out.println("from sout ready handler is called");
-        LOG.info("ready handler is being called");
+        LOG.info("service:==> ready handler is being called");
         httpServletResponse.getWriter().write("1");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        LOG.info("doGet:==> ready handler is being called");
+        httpServletResponse.getWriter().write("1");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        LOG.info("doPost:==> ready handler is being called");
+        httpServletResponse.getWriter().write("1");
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        LOG.info("service(simple):==> ready handler is being called");
+        servletResponse.getWriter().write("1");
     }
 }
