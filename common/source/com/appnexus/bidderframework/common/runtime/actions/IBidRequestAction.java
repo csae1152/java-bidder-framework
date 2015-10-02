fa004f93f2c0225c406bf5c5987f1c8e67face5c
Helmut Steiner <csae1152@uibk.ac.at>
2015-10-02 08:55:47 +0200

    Update IBidRequestAction.java

--- common/source/com/appnexus/bidderframework/common/runtime/actions/IBidRequestAction.java
+++ common/source/com/appnexus/bidderframework/common/runtime/actions/IBidRequestAction.java
@@ -8,7 +8,14 @@ import com.appnexus.bidderframework.common.dataobjects.BidResponse;
  * User: Ira Klotzko
  * Date: Apr 21, 2009
  * Time: 9:56:17 AM
+ * 
+ * updated by @author Helmut Steiner
  */
 public interface IBidRequestAction {
     public BidResponse handleBidRequest(BidRequest bidRequest);
+    /**
+     * need to be a string representation because of given format restrictions of
+     * ISOBus limits.
+     */
+    public String getResponseTime(String timeMessure);
 }
