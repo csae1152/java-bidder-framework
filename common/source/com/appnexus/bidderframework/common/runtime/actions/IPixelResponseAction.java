975c2315fdf7b90ca3168d482ee4297d0a822813
Helmut Steiner <csae1152@uibk.ac.at>
2015-10-06 08:44:25 +0200

    Update IPixelResponseAction.java

--- common/source/com/appnexus/bidderframework/common/runtime/actions/IPixelResponseAction.java
+++ common/source/com/appnexus/bidderframework/common/runtime/actions/IPixelResponseAction.java
@@ -7,7 +7,12 @@ import com.appnexus.bidderframework.common.dataobjects.PixelResponse;
  * User: Ira Klotzko
  * Date: Apr 21, 2009
  * Time: 10:07:22 AM
+ * 
+ * updated by @author Helmut Steiner
+ * 
  */
 public interface IPixelResponseAction {
     void handlePixelResponse(PixelResponse pixelResponse);
+    public String getBidderInfo(String bidderInfo);
+    public int calculateTimeout(int bidderPlatformTimeout);
 }
