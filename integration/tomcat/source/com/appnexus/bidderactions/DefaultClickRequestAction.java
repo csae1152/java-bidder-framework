55bcf2884787708bf24b2b80331bb06e7210d023
Helmut Steiner <csae1152@uibk.ac.at>
2015-10-07 08:42:41 +0200

    Update DefaultClickRequestAction.java

--- integration/tomcat/source/com/appnexus/bidderactions/DefaultClickRequestAction.java
+++ integration/tomcat/source/com/appnexus/bidderactions/DefaultClickRequestAction.java
@@ -8,9 +8,12 @@ import com.appnexus.bidderframework.common.dataobjects.ClickRequest;
  * User: Ira Klotzko
  * Date: Apr 21, 2009
  * Time: 12:17:27 PM
+ * 
+ * @updated by @author Helmut Steiner
+ * 
  */
 public class DefaultClickRequestAction implements IClickRequestAction {
     public void handleClickRequest(ClickRequest clickRequest) {
-
+    public String getTractorConnectionString(String id)
     }
 }
