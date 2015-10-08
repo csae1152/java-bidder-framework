003aad3d327ea884f4eebf9ce979eb3e078a1c04
Helmut Steiner <csae1152@uibk.ac.at>
2015-10-08 09:04:44 +0200

    Update DefaultNotifyRequestAction.java

--- integration/tomcat/source/com/appnexus/bidderactions/DefaultNotifyRequestAction.java
+++ integration/tomcat/source/com/appnexus/bidderactions/DefaultNotifyRequestAction.java
@@ -8,9 +8,11 @@ import com.appnexus.bidderframework.common.dataobjects.NotifyRequest;
  * User: Ira Klotzko
  * Date: Apr 21, 2009
  * Time: 12:17:49 PM
+ * 
+ * updated @author Helmut Steiner
+ * 
  */
 public class DefaultNotifyRequestAction implements INotifyRequestAction {
-    public void handleNotifyRequest(NotifyRequest clickRequest) {
-
-    }
+    public void handleNotifyRequest(NotifyRequest clickRequest) {}
+    public void handleDeviceReques(TractorRequest tractorRequest){}
 }
