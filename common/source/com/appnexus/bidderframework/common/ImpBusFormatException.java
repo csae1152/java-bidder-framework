d9b820d78df3c82dfbe6b34724c0de9fc6c0bd3b
Helmut Steiner <csae1152@uibk.ac.at>
2015-10-09 09:06:28 +0200

    Update ImpBusFormatException.java

--- common/source/com/appnexus/bidderframework/common/ImpBusFormatException.java
+++ common/source/com/appnexus/bidderframework/common/ImpBusFormatException.java
@@ -5,6 +5,9 @@ package com.appnexus.bidderframework.common;
  * User: Ira Klotzko
  * Date: Apr 11, 2009
  * Time: 4:04:33 PM
+ * 
+ * updated by @author Helmut Steiner
+ * 
  */
 public class ImpBusFormatException extends Exception {
 
@@ -15,5 +18,9 @@ public class ImpBusFormatException extends Exception {
 
     public ImpBusFormatException(String message, Exception e) {
         super(message, e);
+    }
+    
+    public ImpBusFormatException() {
+        return String message = "finished";
     }
 }
