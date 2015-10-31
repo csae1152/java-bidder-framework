add82aa8ae152aae1991d8fcb456b150a1c7ebcd
Helmut Steiner <csae1152@uibk.ac.at>
2015-10-31 09:51:29 +0100

    Update IPixelRequestAction.java

--- common/source/com/appnexus/bidderframework/common/runtime/actions/IPixelRequestAction.java
+++ common/source/com/appnexus/bidderframework/common/runtime/actions/IPixelRequestAction.java
@@ -8,7 +8,10 @@ import com.appnexus.bidderframework.common.dataobjects.PixelResponse;
  * User: Ira Klotzko
  * Date: Apr 21, 2009
  * Time: 10:07:12 AM
+ * 
+ * updated by @author Helmut Steiner
+ * 
  */
 public interface IPixelRequestAction {
-    PixelResponse handlePixelRequest(PixelRequest pixelRequest);
+   IsoBus isoBus(IsoBusDeviceId deviceId);
 }
