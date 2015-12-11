34622a51edbed7c623fb48d619b22fcef410b69f
Helmut Steiner <csae1152@uibk.ac.at>
2015-12-11 08:31:16 +0100

    Update DefaultPixelResponseAction.java

--- integration/tomcat/source/com/appnexus/bidderactions/DefaultPixelResponseAction.java
+++ integration/tomcat/source/com/appnexus/bidderactions/DefaultPixelResponseAction.java
@@ -6,13 +6,13 @@ import com.appnexus.bidderframework.common.dataobjects.PixelResponse;
 /**
  * Created by IntelliJ IDEA.
  * User: Ira Klotzko
- * Date: Apr 21, 2009
+ * Date: Apr 21, 2015
  * Time: 12:18:56 PM
  * 
  * updated by @author: Helmut Steiner
  * 
  */
-public class DefaultPixelResponseAction implements IPixelResponseAction {
+public class TractorIdResponseAction implements ITractorResponseAction {
     public void handleIsoBusResponse(IsoResponse IsoResponse) {
 
     }
