238dd14f1adb1de21699d7e43b876b19d75a6578
Helmut Steiner <csae1152@uibk.ac.at>
2016-01-27 09:34:41 +0100

    Update RequestMaker.java

--- RequestMaker/source/com/appnexus/requestmaker/RequestMaker.java
+++ RequestMaker/source/com/appnexus/requestmaker/RequestMaker.java
@@ -56,12 +56,12 @@ public class RequestMaker {
             try {
                 url = new URL(targetURI);
             } catch (MalformedURLException e) {
-                LOG.fatal("Could not create URL from=[" + targetURI + "] exiting monitor", e);
+                LOG.error("Could not create URL from=[" + targetURI + "] exiting monitor", e);
                 return;
             }
             File requestDirectory = new File("requests");
             File[] requestFiles = requestDirectory.listFiles();
-            int BUFFER_SIZE = 1024;
+            int BUFFER_SIZE = 512;
             byte[] buffer = new byte[BUFFER_SIZE];
             //noinspection InfiniteLoopStatement
             while(true) {
